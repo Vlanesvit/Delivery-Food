9ce39ddb032e271985bd580a08f4004e1f50ff11
Vlanesvit <vlanesvit@gmail.com>
2021-11-09 01:05:11 +0300

    Update partners.js

--- js/partners.js
+++ js/partners.js
@@ -1,18 +1,19 @@
-const cardsRestaurants = document.querySelector('.cards-restaurants');
+const partners = () => {
+  const cardsRestaurants = document.querySelector('.cards-restaurants');
 
-const renderItems = (data) => {
-  data.forEach((item) => {
-    const { image, kitchen, name, price, products, stars, time_of_delivery } =
-      item;
-    const a = document.createElement('a');
-    a.setAttribute('href', '/restaurant.html');
-    a.classList.add('card', 'card-restaurant');
+  const renderItems = (data) => {
+    data.forEach((item) => {
+      const { image, kitchen, name, price, products, stars, time_of_delivery } =
+        item;
+      const a = document.createElement('a');
+      a.setAttribute('href', '/restaurant.html');
+      a.classList.add('card', 'card-restaurant');
 
-    // Получаем значение dataset
-    a.dataset.products = products;
+      // Получаем значение dataset
+      a.dataset.products = products;
 
-    // Карточка ресторана
-    a.innerHTML = `
+      // Карточка ресторана
+      a.innerHTML = `
         <img src="${image}" alt="${name}" class="card-image" />
         <div class="card-text">
           <div class="card-heading">
@@ -29,27 +30,32 @@ const renderItems = (data) => {
         </div>
       `;
 
-    a.addEventListener('click', (e) => {
-      e.preventDefault();
-      localStorage.setItem('restaurant', JSON.stringify(item));
+      a.addEventListener('click', (e) => {
+        e.preventDefault();
+        localStorage.setItem('restaurant', JSON.stringify(item));
 
-      // Проверка на авторизованность пользователя перед переходом к странице с меню
-      if (localStorage.getItem('user')) {
-        window.location.href = '/restaurant.html';
-      } else {
-        modalAuth.style.display = 'flex';
-      }
+        // Проверка на авторизованность пользователя перед переходом к странице с меню
+        if (localStorage.getItem('user')) {
+          window.location.href = '/restaurant.html';
+        } else {
+          modalAuth.style.display = 'flex';
+        }
+      });
+
+      cardsRestaurants.append(a);
     });
+  };
 
-    cardsRestaurants.append(a);
-  });
+  fetch(
+    'https://delivery-food-test-default-rtdb.firebaseio.com/db/partners.json'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-fetch('https://delivery-food-test-default-rtdb.firebaseio.com/db/partners.json')
-  .then((response) => response.json())
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+partners();
