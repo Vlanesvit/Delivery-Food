18beeb59269326cc11e3bac9ad0fec963482401d
Vlanesvit <vlanesvit@gmail.com>
2021-11-09 00:00:56 +0300

    Update partners.js

--- js/partners.js
+++ js/partners.js
@@ -1,9 +1,48 @@
-const renderItems = (data) => {
-  console.log(data);
+const cardsRestaurants = document.querySelector('.cards-restaurants');
 
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-  }
+const renderItems = (data) => {
+  data.forEach((item) => {
+    const { image, kitchen, name, price, products, stars, time_of_delivery } =
+      item;
+    const a = document.createElement('a');
+    a.setAttribute('href', '/restaurant.html');
+    a.classList.add('card', 'card-restaurant');
+
+    // Получаем значение dataset
+    a.dataset.products = products;
+
+    // Карточка ресторана
+    a.innerHTML = `
+        <img src="${image}" alt="${name}" class="card-image" />
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title">${name}</h3>
+            <span class="card-tag tag">${time_of_delivery} мин</span>
+          </div>
+          <div class="card-info">
+            <div class="rating">
+              ${stars}
+            </div>
+            <div class="price">От ${price}</div>
+            <div class="category">${kitchen}</div>
+          </div>
+        </div>
+      `;
+
+    a.addEventListener('click', (e) => {
+      e.preventDefault();
+      localStorage.setItem('restaurant', JSON.stringify(item));
+
+      // Проверка на авторизованность пользователя перед переходом к странице с меню
+      if (localStorage.getItem('user')) {
+        window.location.href = '/restaurant.html';
+      } else {
+        modalAuth.style.display = 'flex';
+      }
+    });
+
+    cardsRestaurants.append(a);
+  });
 };
 
 fetch('https://delivery-food-test-default-rtdb.firebaseio.com/db/partners.json')
